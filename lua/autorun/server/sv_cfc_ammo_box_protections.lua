6839f125c227ae9e905b424d64ff2011e32f5415
PLlally <11771225+PLally@users.noreply.github.com>
2020-03-19 16:53:54 -0400

    prevent ammo boxes from killing others

---
+++ lua/autorun/server/sv_cfc_ammo_box_protections.lua
@@ -0,0 +1,20 @@
+local function canACFDamage( activated, targetEnt, energy, fraera, angle, inflictor, bone, gun )
+    if not IsValid( targetEnt ) then return end
+    if not IsValid( inflictor ) then return end
+
+    local targetPerson
+    if targetEnt:IsPlayer() then
+        targetPerson = targetEnt
+    else
+        targetPerson = targetEnt:CPPIGetOwner()
+    end
+
+    if not IsValid( targetPerson ) then return end
+
+    if gun:GetClass() == "acf_ammo" and targetPerson ~= inflictor then
+        return false
+    end
+end
+
+hook.Add( "ACF_BulletDamage", "CFC_PvP_CanACFDamage", canACFDamage )
+
